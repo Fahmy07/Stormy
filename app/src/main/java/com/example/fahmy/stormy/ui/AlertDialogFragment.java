ece90f3d003fcbc3cdc061c60a9ec6f47b8249b8
Fahmy <AhmedFahmy04@gmail.com>
2015-09-22 17:24:14 +0200

    Updating the data model.

--- app/src/main/java/com/example/fahmy/stormy/ui/AlertDialogFragment.java
+++ app/src/main/java/com/example/fahmy/stormy/ui/AlertDialogFragment.java
@@ -1,10 +1,12 @@
-package com.example.fahmy.stormy;
+package com.example.fahmy.stormy.ui;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.os.Bundle;
+
+import com.example.fahmy.stormy.R;
 
 /**
  * Created by hp on 9/6/2015.
---
+++ app/src/main/java/com/example/fahmy/stormy/weather/Forecast.java
@@ -0,0 +1,34 @@
+package com.example.fahmy.stormy.weather;
+
+/**
+ * Created by hp on 9/22/2015.
+ */
+public class Forecast {
+    private Current mCurrent;
+    private Hour[] mHourlyForecast;
+    private Day[] mDailyForecast;
+
+    public Current getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(Current current) {
+        mCurrent = current;
+    }
+
+    public Hour[] getHourlyForecast() {
+        return mHourlyForecast;
+    }
+
+    public void setHourlyForecast(Hour[] hourlyForecast) {
+        mHourlyForecast = hourlyForecast;
+    }
+
+    public Day[] getDailyForecast() {
+        return mDailyForecast;
+    }
+
+    public void setDailyForecast(Day[] dailyForecast) {
+        mDailyForecast = dailyForecast;
+    }
+}
