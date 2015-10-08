945d3cb9a317e83fa01bdd256368a5be429e96f1
Fahmy <AhmedFahmy04@gmail.com>
2015-10-08 10:11:38 +0200

    Acting on List Item Taps.

--- app/src/main/java/com/example/fahmy/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/example/fahmy/stormy/ui/DailyForecastActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.fahmy.stormy.R;
 import com.example.fahmy.stormy.adapters.DayAdapter;
@@ -34,4 +36,17 @@ public class DailyForecastActivity extends ListActivity {
         setListAdapter(adapter);
     }
 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+        String conditions = mDays[position].getSummary();
+        String highTemp = mDays[position].getTemperatureMax() + "";
+        String message = String.format("On %s the high will be %s and it will be %s",
+                    dayOfTheWeek,
+                    highTemp,
+                    conditions);
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
 }
